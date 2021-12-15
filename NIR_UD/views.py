2cb7411650d3a009bd741b3d9a6d3ea33cf97de8
olya_sick <russiandirectioner69@yandex.ru>
2021-12-15 16:00:02 +0300

    Changes by qubel

--- NIR_UD/views.py
+++ NIR_UD/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.models import User
 def index(request):
     return render(request, 'NIR_UD/index.html')
 
+
 def about(request):
     return render(request, 'NIR_UD/about.html')
 
