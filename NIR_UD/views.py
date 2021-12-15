d0bf0664c6a196500a2292309d429e91fbe0a37e
olya_sick <russiandirectioner69@yandex.ru>
2021-12-15 15:59:40 +0300

    Добавлены группы пользователей, права доступа пользователей, созданы пользователи обеих групп

--- NIR_UD/views.py
+++ NIR_UD/views.py
@@ -1,8 +1,65 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.models import User
+
 
 def index(request):
     return render(request, 'NIR_UD/index.html')
 
 def about(request):
     return render(request, 'NIR_UD/about.html')
+
+
+# Права доступа к таблице средних баллов
+view_as_permission = Permission.objects.get(codename='view_averagescore')
+
+# Права доступа к таблице академической успеваемости
+view_ap_permission = Permission.objects.get(codename='view_academicperformance')
+add_ap_permission = Permission.objects.get(codename='add_academicperformance')
+delete_ap_permission = Permission.objects.get(codename='delete_academicperformance')
+change_ap_permission = Permission.objects.get(codename='change_academicperformance')
+
+# Права доступа к расписанию уроков
+view_t_permission = Permission.objects.get(codename='view_timetable')
+
+# Права доступа к таблице домашних заданий
+view_hw_permission = Permission.objects.get(codename='view_homework')
+add_hw_permission = Permission.objects.get(codename='add_homework')
+delete_hw_permission = Permission.objects.get(codename='delete_homework')
+change_hw_permission = Permission.objects.get(codename='change_homework')
+
+# Права доступа к таблице студентов
+view_st_permission = Permission.objects.get(codename='view_students')
+change_st_permission = Permission.objects.get(codename='change_students')
+
+# Права доступа к таблице классов
+view_cl_permission = Permission.objects.get(codename='view_classes')
+
+# Права доступа к таблице учебных предметов
+view_acsub_permission = Permission.objects.get(codename='view_academicsubjects')
+
+# Права доступа ученика
+students_group, _ = Group.objects.get_or_create(name='Students')
+students_group.permissions.add(view_as_permission, view_ap_permission, view_t_permission,
+                               view_hw_permission)
+
+# Права доступа учителя
+teachers_group, _ = Group.objects.get_or_create(name='Teachers')
+teachers_group.permissions.add(add_ap_permission, view_ap_permission, delete_ap_permission,
+                               change_ap_permission)
+teachers_group.permissions.add(add_hw_permission, view_hw_permission, delete_hw_permission,
+                               change_hw_permission)
+teachers_group.permissions.add(view_acsub_permission, view_cl_permission, view_st_permission, change_st_permission,
+                               view_t_permission, view_as_permission)
+
+
+# Создание пользователя группы "ученики"
+student_1 = User.objects.create_user('iov14032008', '12345678')
+students_group.user_set.add(student_1)
+
+# Создание пользователя группы "учителя"
+teacher_1 = User.objects.create_user('isv05071964', '12345678')
+teachers_group.user_set.add(teacher_1)
