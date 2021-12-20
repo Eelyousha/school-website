528b806d55e52384721bce144206e6f5d2a702d7
Ilyoushaa <qubelord777@gmail.com>
2021-12-20 22:36:41 +0300

    Добавил скрипт для создания записей в Academic Performance

---
+++ NIR_UD/management/commands/ap_filler.py
@@ -0,0 +1,44 @@
+from random import choice
+
+from django.core.management.base import BaseCommand
+from faker import Faker
+
+from NIR_UD.models import *
+
+fake = Faker()
+
+
+class Command(BaseCommand):
+    def answers(self, cnt):
+        students_ids = list(
+            Students.objects.values_list('id', flat=True))
+
+        classes_ids = list(
+            Classes.objects.values_list('id', flat=True))
+
+        subject_ids = list(
+            AcademicSubjects.objects.values_list('id', flat=True))
+        print(students_ids)
+        print(classes_ids)
+        print(subject_ids)
+
+        answers = []
+        for _ in range(cnt):
+            subject_id = choice(subject_ids)
+            students_id = choice(students_ids)
+            classes_id = choice(classes_ids)
+            answers.append(AcademicPerformance(
+                lesson_date='.'.join([str(fake.random_int(min=1, max=32)),
+                                      str(fake.random_int(min=1, max=13)),
+                                      str(fake.random_int(min=2020, max=2022))]),
+                is_appeared=fake.pybool(),
+                student_id_id=students_id,
+                class_id_id=classes_id,
+                subject_id_id=subject_id,
+                student_mark=fake.random_int(min=2, max=6)
+            ))
+        AcademicPerformance.objects.bulk_create(answers)
+
+    def handle(self, *args, **options):
+        size = [20, 10001, 100001, 1000001]
+        self.answers(size[0])
